96662f8d5dab5cd3dfe5b5e8f7bdbd9a9f958d02
Matheus Duncan <duncanmatheus905@gmail.com>
2023-09-11 17:18:23 -0600

    added final version of python script.

--- req.py
+++ req.py
@@ -1,4 +1,6 @@
 import requests
-r = requests.get('http://www.google.com')
 print(requests.__version__)
+r = requests.get('http://www.google.com')
 print(r)
+sc = requests.get('https://raw.githubusercontent.com/Matheus-Du/cmput404-lab1/main/req.py')
+print(sc.text)
